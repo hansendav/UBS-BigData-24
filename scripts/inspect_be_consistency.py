b273962cf924ab8cc543f3d8a65faa0af10ecc75
hansendav <davhansen@web.de>
2024-10-15 16:00:04 +0200

    Test

---
+++ scripts/inspect_be_consistency.py
@@ -0,0 +1,34 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder\
+        .master("yarn")\
+        .appName("read_parquet_file_bigearth")\
+        .getOrCreate()
+
+df_s1 = spark.read.format("binaryFile")\
+    .option("recursiveFileLookup")\
+    .option("pathGlobFilter", f"*{type}")\
+    .load("s3://ubs-datasets/bigearthnet/BigEarthNet-S1/")\
+    .select("path")
+
+df_s2 = spark.read.format("binaryFile")\
+    .option("recursiveFileLookup")\
+    .option("pathGlobFilter", f"*{type}")\
+    .load("s3://ubs-datasets/bigearthnet/BigEarthNet-S2/")\
+    .select("path")
+
+
+df_ref_maps = spark.read.format("binaryFile")\
+    .option("recursiveFileLookup")\
+    .option("pathGlobFilter", f"*{type}")\
+    .load("s3://ubs-datasets/bigearthnet/Reference_Maps/")\
+    .select("path")
+
+df_parquet = spark.read.parquet("s3://ubs-datasets/bigearthnet/metadata.parquet")\
+            .limit(5)
+
+df_parquet.printSchema()
+print(f"Number of patches in the dataset: {df.count()}")
+
+print(f"Display of the top 5 rows of the dataframe:")
+df_parquet.show(5)
--- scripts/load_bigearthnet.py
+++ scripts/load_bigearthnet.py
@@ -5,6 +5,20 @@ spark = SparkSession.builder\
         .appName("big_earth_net_to_dataframe")\
         .getOrCreate() 
 
+def load_data_frame(path, format="binaryFile", type='.tif', recursive=True):
+    """Loads a dataframe from a given path with a given format and type.
+    """
+    if recursive == True:
+        df = spark.read.format(format)\
+            .option("recursiveFileLookup")\
+            .option("pathGlobFilter", f"*{type}")\
+            .load(path)
+    else: 
+        df = spark.read.format(format)\
+            .option("pathGlobFilter", f"*{type}")\
+            .load(path)
+    return df
+
 df_s1 = spark.read.format("binaryFile")\
     .load("s3://ubs-datasets/bigearthnet/BigEarthNet-S1/")
 
@@ -22,7 +36,7 @@ for df in dfs:
     print("Schema of the dataframe:")
     df.printSchema()
     print("Adding the shape of the dataframe to the list")
-    rows, columns = df.count(), len(df.columns)
+    rows, columns = df.count(), len(df.columns) # cols does not make any sense here -> is always the same 
     shapes.append((rows, columns))
 
 print("Shapes of the dataframes:")
