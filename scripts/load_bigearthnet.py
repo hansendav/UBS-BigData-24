a863d4e31d628574bf082beeba674d802cb5ce06
hansendav <davhansen@web.de>
2024-10-15 14:55:52 +0200

    testing

--- scripts/load_bigearthnet.py
+++ scripts/load_bigearthnet.py
@@ -1,13 +1,20 @@
 from pyspark.sql import SparkSession  
 
 spark = SparkSession.builder\
-        .master("local")\
+        .master("yarn")\
         .appName("big_earth_net_to_dataframe")\
         .getOrCreate() 
 
 df = spark.read.format("binaryFile")\
-    .load("s3://ubs-datasets/bigearthnet/BigEarthNet-S2/S2A_MSIL2A_20170613T101031_N9999_R022_T33UUP/S2A_MSIL2A_20170613T101031_N9999_R022_T33UUP_26_57/S2A_MSIL2A_20170613T101031_N9999_R022_T33UUP_26_57_B01.tif")
+    .load("s3://ubs-datasets/bigearthnet/BigEarthNet-S2/")
 
 
+print("Schema of the dataframe:")
 df.printSchema()
-df.show()
+
+print("Show the dataframe:")
+df.show() 
+
+print("Dataframe shape") 
+print(f"Number of rows: {df.count()}")
+print(f"Number of columns: {len(df.columns)}")
