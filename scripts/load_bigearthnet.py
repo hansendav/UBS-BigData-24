126ad3698ce23dca7cb55560578619ff8864063d
hansendav <davhansen@web.de>
2024-10-15 14:40:40 +0200

    Test

--- scripts/load_bigearthnet.py
+++ scripts/load_bigearthnet.py
@@ -1,4 +1,13 @@
 from pyspark import SparkContext 
-sc = SparkContext(master='local[4]') # specify here based on the cluster 
-sc.appName = 'load_bigearthnet_to_dataframe'
-print(sc.appName)
+
+spark = SparkSession.builder
+    .master("local")
+    .appName("big_earth_net_to_dataframe")
+    .getOrCreate() 
+
+df = spark.read.format("binaryFile").\
+    load("s3://ubs-datasets/bigearthnet/BigEarthNet-S2/S2A_MSIL2A_20170613T101031_N9999_R022_T33UUP/S2A_MSIL2A_20170613T101031_N9999_R022_T33UUP_26_57/S2A_MSIL2A_20170613T101031_N9999_R022_T33UUP_26_57_B01.tif")
+
+
+df.printSchema()
+df.show()
