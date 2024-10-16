ffeb1d31312ea3552a71d4049c475f9997615f3e
hansendav <davhansen@web.de>
2024-10-16 16:22:36 +0200

    123

--- scripts/pyarrow_s3.py
+++ scripts/pyarrow_s3.py
@@ -1,9 +1,15 @@
 import pyarrow.fs as fs
-
 import pyarrow.parquet as pq
 import pandas as pd
 
 
 parquet_path = "s3://ubs-datasets/bigearthnet/metadata.parquet"
 meta = pq.read_table(parquet_path).to_pandas()
-print(meta)
+
+s3 = fs.S3FileSystem()
+
+s1_file_info = s3.get_file_info(fs.FileSelector(
+   'ubs-datasets/bigearthnetBigEarthNet-S1/', recursive=True
+))
+
+print(s1_file_info)
