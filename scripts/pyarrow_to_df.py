db6832a28dbb6e3a1f0b8d16e72168618ee19e73
hansendav <davhansen@web.de>
2024-10-16 16:14:24 +0200

    Changes

--- scripts/pyarrow_to_df.py
+++ scripts/pyarrow_to_df.py
@@ -11,8 +11,9 @@ s2_path = "BigEarthNet-S2"
 ref_maps_paths = "Reference_Maps"
 
 def extract_filenames(fs, s3_bucket, s3_path):
-        file_paths = fs.get_file_info(fs.FileSelector(f"{s3_bucket}/{s3_path}", recursive=True))
-        return [info.path.split('/')[-1].split('.')[0] for info in file_paths]
+    file_selector = fs.FileSelector(f"{s3_bucket}/{s3_path}", recursive=True)
+    file_info = fs.get_file_info(file_selector)
+    return [info.path.split('/')[-1].split('.')[0] for info in file_info if info.is_file]
 
 s1_paths = extract_filenames(s3, "ubs-datasets/bigearthnet", s1_path)
 s2_paths = extract_filenames(s3, "ubs-datasets/bigearthnet", s2_path)
