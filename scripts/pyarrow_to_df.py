648359e0ca6c227a914cf4212312a427871b54ec
hansendav <davhansen@web.de>
2024-10-16 16:17:21 +0200

    changes

--- scripts/pyarrow_to_df.py
+++ scripts/pyarrow_to_df.py
@@ -1,4 +1,6 @@
 import pyarrow.fs as fs
+from pyarrow.fs import FileSelector
+
 import pyarrow.parquet as pq
 import pandas as pd
 
@@ -11,9 +13,9 @@ s2_path = "BigEarthNet-S2"
 ref_maps_paths = "Reference_Maps"
 
 def extract_filenames(fs, s3_bucket, s3_path):
-    file_selector = fs.FileSelector(f"{s3_bucket}/{s3_path}", recursive=True)
-    file_info = fs.get_file_info(file_selector)
-    return [info.path.split('/')[-1].split('.')[0] for info in file_info if info.is_file]
+        selector = fs.FileSelector(f"{s3_bucket}/{s3_path}", recursive=True)
+        file_info = fs.get_file_info(selector)
+        return [info.path.split('/')[-1].split('.')[0] for info in file_info if info.is_file]
 
 s1_paths = extract_filenames(s3, "ubs-datasets/bigearthnet", s1_path)
 s2_paths = extract_filenames(s3, "ubs-datasets/bigearthnet", s2_path)
