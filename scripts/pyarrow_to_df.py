7f4be4c966ca3ae2651ddf4fc70bfb6848592d90
hansendav <davhansen@web.de>
2024-10-16 16:09:58 +0200

    Adding filename py

--- scripts/pyarrow_to_df.py
+++ scripts/pyarrow_to_df.py
@@ -1,18 +1,28 @@
-import pyarrow.fs as fs 
-from pyspark.sql import SparkSession
-
-
-spark = SparkSession.builder\
-        .master("yarn")\
-        .appName("pyarrow_to_df")\
-        .getOrCreate()
-s3_bucket = "ubs-datasets"
-s3_path = "bigearthnet/" 
+import pyarrow.fs as fs
+import pyarrow.parquet as pq
+import pandas as pd
 
 s3 = fs.S3FileSystem()
 
-file_paths = s3.get_file_info(fs.FileSelector(f"{s3_bucket}/{s3_path}"),
-                recursive=True)
+# paths to parquet and directories
+parquet_path = "s3://ubs-datasets/bigearthnet/metadata.parquet"
+s1_path = "BigEarthNet-S1"
+s2_path = "BigEarthNet-S2"
+ref_maps_paths = "Reference_Maps"
 
-file_paths = [info.path for info in file_paths]
+def extract_filenames(fs, s3_bucket, s3_path):
+        file_paths = fs.get_file_info(fs.FileSelector(f"{s3_bucket}/{s3_path}", recursive=True))
+        return [info.path.split('/')[-1].split('.')[0] for info in file_paths]
 
+s1_paths = extract_filenames(s3, "ubs-datasets/bigearthnet", s1_path)
+s2_paths = extract_filenames(s3, "ubs-datasets/bigearthnet", s2_path)
+ref_maps_paths = extract_filenames(s3, "ubs-datasets/bigearthnet", ref_maps_paths)
+
+# read parquet file
+meta = pq.read_table(parquet_path).to_pandas()
+
+print(s1_paths[:5])
+print(s2_paths[:5])
+print(ref_maps_paths[:5])     
+
+print(meta)
