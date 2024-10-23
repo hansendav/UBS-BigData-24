067b1d6e89173091272d0bbc17fec18d0a87bb2f
hansendav <davhansen@web.de>
2024-10-23 16:10:26 +0200

    More decorators

--- scripts/mlib_test_workflow.py
+++ scripts/mlib_test_workflow.py
@@ -4,6 +4,22 @@ from pyspark.ml import Pipeline
 from pyspark.ml.regression import LinearRegression
 from pyspark.ml.feature import StringIndexer, OneHotEncoder, VectorAssembler, StandardScaler
 from pyspark.ml.evaluation import RegressionEvaluator
+import time
+
+# Decorators
+
+def log_runtime(task_name):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            print(f"{task_name} started at {start_time}")
+            result = func(*args, **kwargs)
+            end_time = time.time()
+            print(f"{task_name} finished at {end_time}")
+            print(f"Runtime task {task_name}: {end_time - start_time:.2f}")
+            return result 
+        return wrapper
+    return  decorator
 
 def print_start_finish(whatstarted_message):
     def decorator(func):
@@ -15,58 +31,64 @@ def print_start_finish(whatstarted_message):
         return wrapper 
     return decorator
 
+
+@log_runtime
+def main():
 # Set up session
-spark = SparkSession.builder\
-    .appName("CO2-regression-model")\
-    .getOrCreate()
+    spark = SparkSession.builder\
+        .appName("CO2-regression-model")\
+        .getOrCreate()
 
-# Load the data
-path_to_file = 's3://ubs-datasets/CO2/CO2_Emissions_Canada.csv'
-df = spark.read.csv(path_to_file,
-                    sep=',',
-                    header=True,
-                    inferSchema=True)
-
-
-train, test = df.randomSplit([0.8, 0.2])
+    # Load the data
+    path_to_file = 's3://ubs-datasets/CO2/CO2_Emissions_Canada.csv'
+    df = spark.read.csv(path_to_file,
+                        sep=',',
+                        header=True,
+                        inferSchema=True)
 
 
-# define the column dtypes for further processing
-categorical_cols = [field for (field, dtype) in train.dtypes if dtype == "string"]
-numerical_cols = [field for (field, dtype) in train.dtypes if dtype in ["int", "double"]]
-
-# index and encode the categorical columns
-indexers = [StringIndexer(inputCol=col, outputCol=col + "_index", handleInvalid='keep') for col in categorical_cols]
-encoders = [OneHotEncoder(inputCol=col + "_index", outputCol=col + "_ohe") for col in categorical_cols]
-
-# Set numerical assembler for StandardScaling 
-numerical_assembler = VectorAssembler(inputCols=numerical_cols, outputCol="numerical_features")
-scaler = StandardScaler(inputCol="numerical_features", outputCol="scaled_numerical_features") # output
-
-# assemble all features
-assembler = VectorAssembler(inputCols=[col + "_ohe" for col in categorical_cols] + ['scaled_numerical_features'], outputCol="features")
-
-# -> taking again all features as featuresColumns (the general assembler)
-lr = LinearRegression(featuresCol="features", labelCol="CO2")
-
-# indexers and encoders are lists!!! thats why they are not within the brackets 
-pipeline = Pipeline(stages= indexers + encoders + [numerical_assembler, scaler, assembler, lr])
+    train, test = df.randomSplit([0.8, 0.2])
 
 
-# fit the model 
-@print_start_finish("Model training")
-def train_model(pipeline, trainset):
-    lr_model = pipeline.fit(train)
-    return lr_model
+    # define the column dtypes for further processing
+    categorical_cols = [field for (field, dtype) in train.dtypes if dtype == "string"]
+    numerical_cols = [field for (field, dtype) in train.dtypes if dtype in ["int", "double"]]
 
-lr_model = train_model(pipeline, train)
+    # index and encode the categorical columns
+    indexers = [StringIndexer(inputCol=col, outputCol=col + "_index", handleInvalid='keep') for col in categorical_cols]
+    encoders = [OneHotEncoder(inputCol=col + "_index", outputCol=col + "_ohe") for col in categorical_cols]
 
-#  make predictions 
-preds = lr_model.transform(test)
+    # Set numerical assembler for StandardScaling 
+    numerical_assembler = VectorAssembler(inputCols=numerical_cols, outputCol="numerical_features")
+    scaler = StandardScaler(inputCol="numerical_features", outputCol="scaled_numerical_features") # output
 
-# evaluate the model 
-evaluator = RegressionEvaluator(labelCol='CO2', predictionCol='prediction', metricName='rmse')
-rmse = evaluator.evaluate(preds)
+    # assemble all features
+    assembler = VectorAssembler(inputCols=[col + "_ohe" for col in categorical_cols] + ['scaled_numerical_features'], outputCol="features")
 
-# print RMSE of the model 
-print(f"The RMSE of the simple model is: {rmse}")
+    # -> taking again all features as featuresColumns (the general assembler)
+    lr = LinearRegression(featuresCol="features", labelCol="CO2")
+
+    # indexers and encoders are lists!!! thats why they are not within the brackets 
+    pipeline = Pipeline(stages= indexers + encoders + [numerical_assembler, scaler, assembler, lr])
+
+
+    # fit the model 
+    @print_start_finish("Model training")
+    def train_model(pipeline, trainset):
+        lr_model = pipeline.fit(train)
+        return lr_model
+
+    lr_model = train_model(pipeline, train)
+
+    #  make predictions 
+    preds = lr_model.transform(test)
+
+    # evaluate the model 
+    evaluator = RegressionEvaluator(labelCol='CO2', predictionCol='prediction', metricName='rmse')
+    rmse = evaluator.evaluate(preds)
+
+    # print RMSE of the model 
+    print(f"The RMSE of the simple model is: {rmse}")
+
+    spark.stop()
+    print(f"Spark session stopped")
