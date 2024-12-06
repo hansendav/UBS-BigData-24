e88f59ad31f6daf2152d346aeed399d98724526d
hansendav <davhansen@web.de>
2024-12-06 15:02:40 +0100

    2C13EXC16GB

--- m1/bashfiles/spark_training.sh
+++ m1/bashfiles/spark_training.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Configuration
-APP_NAME="m1_rf_sub001_dynamic_2c13exec18GB"
+APP_NAME="m1_rf_sub001_dynamic_2c13exec16GB"
 MASTER="yarn"
 DEPLOY_MODE="cluster"
 SCRIPT_PATH="/home/efs/erasmus/e2405193/ubs_bigdata/UBS-BigData-24/m1/scripts/m1_train_vlocal_transformers.py"
 SUBSAMPLE="0.01"
 NUM_EXECUTORS="13"
 EXECUTOR_CORES="2"
-EXECUTOR_MEMORY="18g"
-DRIVER_MEMORY="18g"
-LOGFILE_NAME="m1_rf_sub001_static_2c14exec18GB.log"
+EXECUTOR_MEMORY="16g"
+DRIVER_MEMORY="16g"
+LOGFILE_NAME="m1_rf_sub001_dynamic_2c13exec16GB.log"
 S3_BUCKET="s3://ubs-cde/home/e2405193/bigdata/log_files"
 
 # Run Spark-submit with nohup and log output to a file
@@ -21,7 +21,7 @@ nohup spark-submit \
     --conf spark.dynamicAllocation.enabled=true \
     --conf spark.dynamicAllocation.minExecutors=2 \
     --conf spark.dynamicAllocation.maxExecutors="$NUM_EXECUTORS" \
-    --conf spark.dynamicAllocation.initialExecutors=4 \
+    --conf spark.dynamicAllocation.initialExecutors=2 \
     --conf spark.shuffle.service.enabled=true \
     --executor-cores "$EXECUTOR_CORES" \
     --executor-memory "$EXECUTOR_MEMORY" \
