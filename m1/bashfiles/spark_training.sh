0496e7ee49e2e63fff4dffbdf2fcbae64feea78f
hansendav <davhansen@web.de>
2024-12-06 09:43:35 +0100

    Adaptation for dynamic test run

--- m1/bashfiles/spark_training.sh
+++ m1/bashfiles/spark_training.sh
@@ -5,12 +5,12 @@ APP_NAME="m1_rf_subs_005"
 MASTER="yarn"
 DEPLOY_MODE="cluster"
 SCRIPT_PATH="/home/efs/erasmus/e2405193/ubs_bigdata/UBS-BigData-24/m1/scripts/m1_train_vlocal_transformers.py"
-SUBSAMPLE="0.05"
-NUM_EXECUTORS="8"
+SUBSAMPLE="0.01"
+NUM_EXECUTORS="5"
 EXECUTOR_CORES="5"
-EXECUTOR_MEMORY="29g"
-DRIVER_MEMORY="29g"
-LOGFILE_NAME="m1_rf_subs005.log"
+EXECUTOR_MEMORY="40g"
+DRIVER_MEMORY="40g"
+LOGFILE_NAME="m1_rf_subs001_dynamic_test.log"
 S3_BUCKET="s3://ubs-cde/home/e2405193/bigdata/log_files"
 
 # Run Spark-submit with nohup and log output to a file
@@ -20,7 +20,7 @@ nohup spark-submit \
     --deploy-mode "$DEPLOY_MODE" \
     --conf spark.dynamicAllocation.enabled=true \
     --conf spark.dynamicAllocation.minExecutors=2 \
-    --conf spark.dynamicAllocation.maxExecutors=5 \
+    --conf spark.dynamicAllocation.maxExecutors="$NUM_EXECUTORS" \
     --conf spark.dynamicAllocation.initialExecutors=2 \
     --conf soark.shuffle.service.enabled=true \
     --num-executors "$NUM_EXECUTORS" \
